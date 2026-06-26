9b31baf0f1c29b20b34e4c078d04d96f55b4e55c
marcella <marcellamatos2428@gmail.com>
2026-06-26 14:45:46 -0300

    estrutura do codigo

--- js/script_comercio.js
+++ js/script_comercio.js
@@ -1,13 +1,90 @@
-const form = document.querySelector('#formproduto')
+const form = document.querySelector("#formproduto");
 
-let comercio = [];
+let produtos = [];
 
-form.addEventListener('submit', (evt) => {
+form.addEventListener("submit", (evt) => {
+
     evt.preventDefault();
 
-    const descricao = document.querySelector('#descricao').value;
-    const valorunitario =  Number (document.querySelector('#valor unitario').value);
-    const quantidade = Number (document.querySelector("#quantidade").value);
+let descricao = document.querySelector("#descricao").value;
 
-})
+let valor = Number(document.querySelector("#valor").value);
 
+let quantidade = Number(document.querySelector("#quantidade").value);
+
+let total = valor * quantidade;
+
+let adicional = calcularAdicional(total);
+
+let produtos = [];
+
+descricao,
+valor,
+quantidade,
+total,
+adicional
+
+});
+
+mostrarProdutos();
+
+form.reset();
+
+function calcularAdicional(total){
+
+if(total <= 3000){
+
+return "Isento";
+
+}
+
+else if(total <= 8000){
+
+return "R$ " + (total * 0.05).toFixed(2);
+
+}
+
+else if(total <= 12000){
+
+return "R$ " + (total * 0.10).toFixed(2);
+
+}
+
+else if(total <= 20000){
+
+return "R$ " + (total * 0.15).toFixed(2);
+
+}
+
+else{
+
+return "R$ " + (total * 0.20).toFixed(2);
+
+}
+
+}
+
+function mostrarProdutos(){
+
+let tabela = document.querySelector("#listaprodutos");
+
+tabela.innerHTML = "";
+
+for(let i = 0; i < produtos.length; i++){
+
+    tabela.innerHTML += `
+    <p>
+    Descrição: ${produtos[i].descricao}<br>
+    Valor: R$ ${produtos[i].valor.toFixed(2)}<br>
+    Quantidade: ${produtos[i].quantidade}<br>
+    Total: R$ ${produtos[i].total.toFixed(2)}<br>
+    Adicional: ${produtos[i].adicional}
+    </p>
+    <hr>
+    `;
+    
+
+}
+
+
+}
