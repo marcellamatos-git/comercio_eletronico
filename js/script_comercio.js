f3d69285a17e99cb28618b1d6bb14a117d055663
marcella <marcellamatos2428@gmail.com>
2026-06-26 14:02:56 -0300

    formulario

---
+++ js/script_comercio.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#formproduto')
+
+let comercio = [];
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const descricao = document.querySelector('#descricao').value;
+    const valorunitario =  Number (document.querySelector('#valor unitario').value);
+    const quantidade = Number (document.querySelector("#quantidade").value);
+
+})
+
